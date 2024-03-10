2f921dff5b18af8db98570845d58c438a64d9b92
Stroonn <matheus.gato.gt98@gmail.com>
2024-03-10 14:15:08 -0300

    projeto final

---
+++ src/model/team.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const Project = require('./project');
+
+const Team = sequelize.define("Team", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+Team.belongsTo(Project, {
+  foreignKey: {
+    allowNull: true,
+  },
+  onDelete: 'SET NULL',
+})
+
+sequelize.sync({ force: true }).then(() => {
+  console.log('Database synchronized');
+}).catch((err) => {
+  console.error('Error synchronizing database:', err);
+});
+
+module.exports = Team;
